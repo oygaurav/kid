dab6f61a739b191ba52be201a18f2cc33504ec83
oygaurav <157347962+oygaurav@users.noreply.github.com>
2024-11-20 11:42:10 +0530

    added

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+// navbar starts
+
+document.querySelector('.navbar-toggler').addEventListener('click', function () {
+    const navbar = document.getElementById('navbarsExample01');
+    navbar.classList.toggle('open');
+});
+
+// navbar ends
